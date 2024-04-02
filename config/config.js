9187ce8aaea7da11860e24eac7ff213594d1f41e
samuel00410 <samuel00410@gmail.com>
2024-04-02 13:47:18 +0800

    make change for deployment

--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+let url = process.env.CLEARDB_DATABASE_URL;
+let match = url.match(/mysql:\/\/([^:]+):([^@]+)@([^\/]+)\/([^?]+)/);
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
@@ -22,10 +25,10 @@ module.exports = {
     timezone: "+08:00", // 添加這行來設置時區為 UTC+8
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: match[1],
+    password: match[2],
+    database: match[4],
+    host: match[3],
     dialect: "mysql",
     charset: "utf8mb4",
     collate: "utf8mb4_unicode_ci",
