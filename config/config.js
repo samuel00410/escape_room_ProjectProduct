e8599b01cfb9d84b03fa4d41eb8adb6e344e4566
samuel00410 <samuel00410@gmail.com>
2024-04-02 13:25:12 +0800

    make changes for deployment ver.2

--- config/config.js
+++ config/config.js
@@ -12,20 +12,20 @@ module.exports = {
     timezone: "+08:00", // 添加這行來設置時區為 UTC+8
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     charset: "utf8mb4",
     collate: "utf8mb4_unicode_ci",
     timezone: "+08:00", // 添加這行來設置時區為 UTC+8
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "mysql",
     charset: "utf8mb4",
     collate: "utf8mb4_unicode_ci",
