996061f4576cf16cb3c90a40f70378a70d8dd2b1
samuel00410 <samuel00410@gmail.com>
2024-12-03 19:49:58 +0800

    重新上傳

--- routes/auth.js
+++ routes/auth.js
@@ -119,51 +119,51 @@ router.get("/logout", (req, res) => {
 });
 
 // Google方式登入
-// router.get(
-//   "/google",
-//   passport.authenticate("google", {
-//     scope: ["profile", "email"],
-//     prompt: "select_account",
-//   })
-// );
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    prompt: "select_account",
+  })
+);
 
 // Google的重新導向URI
-// router.get(
-//   "/google/redirect",
-//   passport.authenticate("google", {
-//     failureRedirect:
-//       "https://escaperoom-project-6a9688f7a146.herokuapp.com/login",
-//   }),
-//   (req, res) => {
-//     return res.redirect(
-//       "https://escaperoom-project-6a9688f7a146.herokuapp.com/"
-//     );
-//   }
-// );
+router.get(
+  "/google/redirect",
+  passport.authenticate("google", {
+    failureRedirect:
+      "https://escaperoom-project-6a9688f7a146.herokuapp.com/login",
+  }),
+  (req, res) => {
+    return res.redirect(
+      "https://escaperoom-project-6a9688f7a146.herokuapp.com/"
+    );
+  }
+);
 
 // Facebook方式登入
-// router.get(
-//   "/facebook",
-//   passport.authenticate("facebook", {
-//     scope: "email",
-//     prompt: "select_account",
-//   })
-// );
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", {
+    scope: "email",
+    prompt: "select_account",
+  })
+);
 
 // Facebook的重新導向URI
-// router.get(
-//   "/facebook/redirect",
-//   passport.authenticate("facebook", {
-//     failureRedirect:
-//       "https://escaperoom-project-6a9688f7a146.herokuapp.com/login",
-//   }),
-//   (req, res) => {
-//     // 登入成功，重定向到主頁面
-//     return res.redirect(
-//       "https://escaperoom-project-6a9688f7a146.herokuapp.com/"
-//     );
-//   }
-// );
+router.get(
+  "/facebook/redirect",
+  passport.authenticate("facebook", {
+    failureRedirect:
+      "https://escaperoom-project-6a9688f7a146.herokuapp.com/login",
+  }),
+  (req, res) => {
+    // 登入成功，重定向到主頁面
+    return res.redirect(
+      "https://escaperoom-project-6a9688f7a146.herokuapp.com/"
+    );
+  }
+);
 
 // 檢查當前用戶狀態。這個 API 應該能夠識別用戶是通過 JWT 還是 Session登入的
 router.get("/current_user", async (req, res) => {
